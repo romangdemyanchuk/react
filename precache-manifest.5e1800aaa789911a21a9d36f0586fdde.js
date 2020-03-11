d465e52f9338cfc4283505d82f74c6ecd774a98e
Roman <roman.g.demyanchuk@gmail.com>
2020-03-11 22:32:33 +0200

    Updates

---
+++ precache-manifest.5e1800aaa789911a21a9d36f0586fdde.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb75d454a0263bf36a338b45cc684eef",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "047956d1fba7eb502735",
+    "url": "/react/static/css/main.507db4f4.chunk.css"
+  },
+  {
+    "revision": "f4cd937c13083751f6d5",
+    "url": "/react/static/js/2.ef7f0626.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/react/static/js/2.ef7f0626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "047956d1fba7eb502735",
+    "url": "/react/static/js/main.7dc3029e.chunk.js"
+  },
+  {
+    "revision": "c6dca7afcdc78aa4b5b0",
+    "url": "/react/static/js/runtime-main.c7016516.js"
+  },
+  {
+    "revision": "947169e955d5ee09cfe6bc962bac56ec",
+    "url": "/react/static/media/Comp.947169e9.jpg"
+  },
+  {
+    "revision": "209315a82dcf0c3dfd7e92031e36f149",
+    "url": "/react/static/media/Lingua desktop 2.209315a8.png"
+  },
+  {
+    "revision": "9e050576d2285968184f54fb94e20867",
+    "url": "/react/static/media/Lingua phone 1.9e050576.png"
+  },
+  {
+    "revision": "39569cc2758d9b23a8621a948cf8eca9",
+    "url": "/react/static/media/Lingua phone 2.39569cc2.png"
+  },
+  {
+    "revision": "8856bc80a5655badb2c29dc99ab9c4b9",
+    "url": "/react/static/media/Lingua phone 3.8856bc80.png"
+  },
+  {
+    "revision": "8abe3dd4f2df850dd2b46e2d32ca62e4",
+    "url": "/react/static/media/Lingua phone 4.8abe3dd4.png"
+  },
+  {
+    "revision": "d85c74dc7583a56ee4fb08728e9ef61c",
+    "url": "/react/static/media/Lingua phone 5.d85c74dc.png"
+  },
+  {
+    "revision": "1b2ba65325d3b802611bc2b5414aecc5",
+    "url": "/react/static/media/Lingua web 1.1b2ba653.png"
+  },
+  {
+    "revision": "0d5c42890c48b3ae615226bcc60b6d4b",
+    "url": "/react/static/media/Lingua web 2.0d5c4289.png"
+  },
+  {
+    "revision": "4e1daf899197fbcb4338c36fa575f31e",
+    "url": "/react/static/media/Lingua web 5.4e1daf89.png"
+  },
+  {
+    "revision": "52bd3b11b2a91b1c5cfaed77e959fec3",
+    "url": "/react/static/media/Lingua web 6.52bd3b11.png"
+  },
+  {
+    "revision": "906bd9903a84621c3fe684e731cda7c6",
+    "url": "/react/static/media/logo1.906bd990.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Lingua-React/precache-manifest.2709dde89823c72b3061d747968f1815.js"
+  "/react/precache-manifest.5e1800aaa789911a21a9d36f0586fdde.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Lingua-React/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
